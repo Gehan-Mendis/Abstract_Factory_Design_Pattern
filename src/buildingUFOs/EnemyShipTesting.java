5a37a5c13c5929a7dbe0a32ecc5f9019ec6a14cc
Gehan Mendis <gehan.mendis@gmail.com>
2021-05-18 18:56:25 +0530

    Initial Commit
    Initial Commit

---
+++ src/buildingUFOs/EnemyShipTesting.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package buildingUFOs;
+
+/**
+ *
+ */
+public class EnemyShipTesting {
+
+	/**
+	 * 
+	 */
+	public EnemyShipTesting() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+public static void main(String[] args) {
+		
+		// EnemyShipBuilding handles orders for new EnemyShips
+		// You send it a code using the orderTheShip method &
+		// it sends the order to the right factory for creation
+	
+		EnemyShipBuilding MakeUFOs = new UFOEnemyShipBuilding();
+ 
+		EnemyShip theGrunt = MakeUFOs.orderTheShip("UFO");
+		System.out.println(theGrunt + "\n");
+		
+		EnemyShip theBoss = MakeUFOs.orderTheShip("UFO BOSS");
+		System.out.println(theBoss + "\n");
+ 
+	}
+
+}
